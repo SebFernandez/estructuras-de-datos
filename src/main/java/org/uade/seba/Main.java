54cf9f0bc6a80dafbd1957a83bcac69ce25ad533
Sebastián Fernandez <seba.fernandez95@gmail.com>
2024-09-08 22:51:09 -0300

    feat: copia de pila-estatica.

--- src/main/java/org/uade/seba/Main.java
+++ src/main/java/org/uade/seba/Main.java
@@ -1,21 +1,6 @@
 package org.uade.seba;
 
-import org.uade.seba.stack.staticImpl.StaticStack;
-
 public class Main {
-    public static void main(String[] args) throws IllegalAccessException {
-        StaticStack<Integer> stack = new StaticStack<>(10);
-        int i = 0;
-
-        while(!stack.isFull()) {
-            stack.push(i);
-            i++;
-        }
-
-
-        while (!stack.isEmpty())
-            stack.pop();
-
-        System.out.println("bye");
+    public static void main(String[] args) {
     }
 }
--- src/main/java/org/uade/seba/stack/Stack.java
+++ src/main/java/org/uade/seba/stack/Stack.java
@@ -2,6 +2,7 @@ package org.uade.seba.stack;
 
 public interface Stack<E> {
 
+    Stack<E> copy() throws IllegalAccessException;
     E getFirst() throws IllegalAccessException;
     E pop() throws IllegalAccessException;
     void push(E value) throws IllegalAccessException;
